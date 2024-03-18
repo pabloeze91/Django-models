4aaef6ac3e6927f3fbb95ca8fe447220410d0825
inoveAlumnos <alumnos@inove.com.ar>
2024-03-18 16:14:44 -0300

    Agregar endpoint para tarea

--- ejercicios_practica/marvel/e_commerce/views.py
+++ ejercicios_practica/marvel/e_commerce/views.py
@@ -5,3 +5,7 @@ from django.http import JsonResponse
 def hola_mundo(request):    
     datos =  {"mensaje": "hola_mundo", "curso": "python-django"}
     return JsonResponse(datos)
+
+def get_comic_api_view(request):
+    print("Endpoint get_comic_api_view")
+    # Alumno, aquí deberá completar el endpoint

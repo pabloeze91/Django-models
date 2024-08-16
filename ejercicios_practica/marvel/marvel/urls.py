f8659e5eb0c70793bc9b1bf7511bd999af9b1b31
Pablo Schlotthauer <126608539+pabloeze91@users.noreply.github.com>
2024-08-16 13:28:09 -0300

    Update urls.py

--- ejercicios_practica/marvel/marvel/urls.py
+++ ejercicios_practica/marvel/marvel/urls.py
@@ -17,6 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from e_commerce import views
 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("hola-mundo/", views.hola_mundo_api_view, name="hola-mundo"),
+    path("get-comic/", views.get_comic_api_view, name="get-comic")
+]
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("hola-mundo/", views.hola_mundo, name="hola-mundo"),

d25493afa46f548496eed6b65403c49e44a2bf4b
Pablo Schlotthauer <126608539+pabloeze91@users.noreply.github.com>
2024-08-16 13:19:11 -0300

    Update views.py

--- ejercicios_practica/marvel/e_commerce/views.py
+++ ejercicios_practica/marvel/e_commerce/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your views here.
-def hola_mundo(request):    
+def hola_mundo_api_view(request):    
     datos =  {"mensaje": "hola_mundo", "curso": "python-django"}
     return JsonResponse(datos)
 
 def get_comic_api_view(request):
-    print("Endpoint get_comic_api_view")
-    # Alumno, aquí deberá completar el endpoint
+    datos = {
+        "id": "1", 
+        "marvel_comic": "1010", 
+        "title": "Inove",
+        "stock_qty": 6,
+        "description": "Mi primer JSON en Django",
+        "price": 10.0,
+        "picture": "https://www.django-rest-framework.org/img/logo.png"        
+    }
+    return JsonResponse(datos)
